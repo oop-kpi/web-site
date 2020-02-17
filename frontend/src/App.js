6792f3d73fb780951f0ed5cd2502275bab0e0c1b
NothingIsMatter <sashalays1337@gmail.com>
2020-02-17 19:06:14 +0200

    Reworked login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,19 +8,41 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import AppBar from "material-ui/AppBar";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
 
 function App() {
+    const classes = useStyles();
   return (
-      <Router>                <MuiThemeProvider>
-        <AppBar
-          title="ООП"
-      >
-          <h3>
-<Link to="/login">Login</Link>
-          </h3>
-        </AppBar> </MuiThemeProvider>
+      <Router>
+          <div className={classes.root}>
+              <AppBar position="static">
+                  <Toolbar >
+                      <Typography variant="h6"  className={classes.title}>
+                          News
+                      </Typography>
+                      <Button href="login" color="inherit">Login</Button>
+                  </Toolbar>
+              </AppBar>
+          </div>
 
         <Switch>
           <Route exact path="/">
