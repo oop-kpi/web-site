abbf1ef165cdc4c1817652af9caf1f5911315a37
NothingIsMatter <sashalays1337@gmail.com>
2020-02-19 13:36:22 +0200

    Add: Lecture upload form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import RegisterPage from "./components/RegisterPage";
 import LecturesPage from "./components/LecturesPage";
-
+import LectureUploadingPage from "./components/LectureUploadingPage";
 const useStyles = makeStyles(theme => ({
     root: {
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -61,6 +61,9 @@ function App() {
           </Route>
             <Route path="/register">
             <RegisterPage></RegisterPage>
+          </Route>
+            <Route path="/uploadLecture">
+            <LectureUploadingPage></LectureUploadingPage>
           </Route>
         </Switch>
       </Router>
